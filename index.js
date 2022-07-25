dff42f65ad4c86cb9a25f7d04e25c4e6270a707c
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-25 12:05:39 +0600

    reducers for redux is added

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function buyJersey() {
     info: 'First redux action to buy jersey',
   };
 }
+
+// reducers are basically used for changing states of application based on action. So here is the demo:
+
+const initialState = { numOfJersies: 500 };
+
+const reducersForJersey = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_JERSEY:
+      return {
+        ...state, // here spread operator copying state values
+        numOfJersies: state.numOfJersies - 3,
+      };
+    default:
+      return state;
+  }
+};
